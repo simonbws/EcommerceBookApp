983a18bbd210522e67fb1ea2312a168f4a302949
szymon <DESKTOP-C0TU3RB>
2022-09-17 13:43:45 +0200

    Add project files.

---
+++ EcommerceBookApp/Database/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EcommerceBookApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceBookApp.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ EcommerceBookApp/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceBookApp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string DisplayOrder { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
