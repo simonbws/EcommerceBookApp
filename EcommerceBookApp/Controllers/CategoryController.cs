b03713f319b77f982aeb336808e4f9b3948bdf0b
szymon <DESKTOP-C0TU3RB>
2022-09-17 19:09:48 +0200

    Dodano Bootswatch, zmieniono navbar oraz layout strony

---
+++ EcommerceBookApp/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using EcommerceBookApp.Database;
+using EcommerceBookApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceBookApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCatList = _db.Categories;
+            return View(objCatList);
+        }
+    }
+}
