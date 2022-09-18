e0fec326075de1c4524ad44aa48edfdbcca00fd5
szymon <DESKTOP-C0TU3RB>
2022-09-18 19:06:18 +0200

    Delete Button and Stamp Data added

--- EcommerceBookApp/Controllers/CategoryController.cs
+++ EcommerceBookApp/Controllers/CategoryController.cs
@@ -27,17 +27,88 @@ namespace EcommerceBookApp.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            if(obj.Name == obj.DisplayOrder.ToString())
+            if (obj.Name == obj.DisplayOrder.ToString())
             {
                 ModelState.AddModelError("CustomError", "The DisplayOrder cannot accept this Name, as they are the same. ");
             }
-            if(ModelState.IsValid)
-            { 
-            _db.Categories.Add(obj); //creating a method that will be pushed to database
-            _db.SaveChanges(); // pushing to database by SaveChanges command
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj); //creating a method that will be pushed to database
+                _db.SaveChanges(); // pushing to database by SaveChanges command
+                TempData["success"] = "Category has been made successfully";
+                return RedirectToAction("Index");
             }
             return View(obj);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot accept this Name, as they are the same. ");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj); //creating a method that will be pushed to database
+                _db.SaveChanges(); // pushing to database by SaveChanges command
+                TempData["success"] = "Category was updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj); //creating a method that will be pushed to database
+            _db.SaveChanges(); // pushing to database by SaveChanges command
+            TempData["success"] = "Category was deleted successfully";
+            return RedirectToAction("Index");
+
+            
         }
 
     }
