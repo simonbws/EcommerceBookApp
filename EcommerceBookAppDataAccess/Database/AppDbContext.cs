d53921a7b492e0698e539f318c2b2155090cbeac
szymon <DESKTOP-C0TU3RB>
2022-09-26 00:08:49 +0200

    CRUD Company Part 1 : Company model, repo and unitofwork added

--- EcommerceBookAppDataAccess/Database/AppDbContext.cs
+++ EcommerceBookAppDataAccess/Database/AppDbContext.cs
@@ -14,5 +14,6 @@ public class AppDbContext : IdentityDbContext
     public DbSet<CoverType> CoverTypes { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<AppUser> AppUsers { get; set; }
+    public DbSet<Company> Companies { get; set; }
 
 }
---
+++ EcommerceBookAppDataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,25 @@
+﻿using EcommerceBookApp.DataAccess.Repository.IRepository;
+using EcommerceBookApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceBookApp.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private AppDbContext _db;
+        public CompanyRepository(AppDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Company obj)
+        {
+            _db.Companies.Update(obj);
+        }
+    }
+}
