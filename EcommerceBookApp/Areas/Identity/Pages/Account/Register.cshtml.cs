07cb06480abcd6688f45a9ce52b6d290550ed3c6
szymon <DESKTOP-C0TU3RB>
2022-09-26 15:33:07 +0200

    more role fields added

--- EcommerceBookApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ EcommerceBookApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using EcommerceBookApp.Models;
 using EcommerceBookApp.Utility;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
@@ -102,6 +103,15 @@ namespace EcommerceBookAppWeb.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
+
         }
 
 
@@ -133,7 +143,13 @@ namespace EcommerceBookAppWeb.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
-                var result = await _userManager.CreateAsync(user, Input.Password);
+                user.StreetAddress = Input.StreetAddress;
+                user.City = Input.City;
+                user.State = Input.State;
+                user.PostalCode = Input.PostalCode;
+                user.Name=Input.Name;
+                user.PhoneNumber = Input.PhoneNumber;
+                var result = await _userManager.CreateAsync(user, Input.Password); //here we create user with the help of usermanager and its helper method
 
                 if (result.Succeeded)
                 {
@@ -171,11 +187,11 @@ namespace EcommerceBookAppWeb.Areas.Identity.Pages.Account
             return Page();
         }
 
-        private IdentityUser CreateUser()
+        private AppUser CreateUser()
         {
             try
             {
-                return Activator.CreateInstance<IdentityUser>();
+                return Activator.CreateInstance<AppUser>();
             }
             catch
             {
