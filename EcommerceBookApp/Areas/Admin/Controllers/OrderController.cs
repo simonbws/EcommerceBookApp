87dd206c901dc7ed5ecf63c59db4909f98313e01
szymon <DESKTOP-C0TU3RB>
2022-10-02 17:51:51 +0200

    Role regarding the order display

--- EcommerceBookApp/Areas/Admin/Controllers/OrderController.cs
+++ EcommerceBookApp/Areas/Admin/Controllers/OrderController.cs
@@ -1,12 +1,15 @@
 ﻿using EcommerceBookApp.DataAccess.Repository.IRepository;
 using EcommerceBookApp.Models;
 using EcommerceBookApp.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Query.Internal;
+using System.Security.Claims;
 
 namespace EcommerceBookAppWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize] // order controller should be only accessible by the authorized user
     public class OrderController : Controller
     {
         private readonly IUnitOfWork _unitOW;
@@ -24,9 +27,21 @@ namespace EcommerceBookAppWeb.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult GetAll(string status)
         {
+            
             IEnumerable<OrderHeader> orderHeaders;
-            orderHeaders = _unitOW.OrderHeader.GetAll(includeProperties: "AppUser");
 
+            if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee)) //admin or employee can see all the order in order to process them
+            {
+                orderHeaders = _unitOW.OrderHeader.GetAll(includeProperties: "AppUser");
+            }
+            else // below they should only see the orders that belong to user id
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity; // here we get the user id of the log in user using claims
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                orderHeaders = _unitOW.OrderHeader.GetAll(u=>u.AppUserId == claim.Value, includeProperties: "AppUser");
+            }
+            
+            //below is where we are retrievieng all the orders
             switch (status)
             {
                 case "pending":
