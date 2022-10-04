2d371ccd4d8a0416e595e1b02e311c6811818d74
szymon <DESKTOP-C0TU3RB>
2022-10-04 19:57:11 +0200

    Order Cancel impl

--- EcommerceBookApp/Areas/Admin/Controllers/OrderController.cs
+++ EcommerceBookApp/Areas/Admin/Controllers/OrderController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Query.Internal;
 using Stripe;
+using Stripe.Checkout;
 using System.Security.Claims;
 
 namespace EcommerceBookAppWeb.Areas.Admin.Controllers
@@ -17,7 +18,6 @@ namespace EcommerceBookAppWeb.Areas.Admin.Controllers
         private readonly IUnitOfWork _unitOW;
         [BindProperty]
         public OrderViewModel OrderViewModel { get; set; }
-
         public OrderController(IUnitOfWork unitOW)
         {
             _unitOW = unitOW;
@@ -35,6 +35,79 @@ namespace EcommerceBookAppWeb.Areas.Admin.Controllers
                 OrderDetail = _unitOW.OrderDetail.GetAll(u => u.OrderId == orderId, includeProperties: "Product"),
             };
             return View(OrderViewModel); // here is get action for details, details is in order js
+        }
+        [ActionName("Details")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DetailsPay() // parameter is taken from order js,routing Details?orderId, now we add details view
+        {
+            OrderViewModel.OrderHeader = _unitOW.OrderHeader.GetFirstOrDefault(u => u.Id == OrderViewModel.OrderHeader.Id, includeProperties: "AppUser");
+            OrderViewModel.OrderDetail = _unitOW.OrderDetail.GetAll(u => u.OrderId == OrderViewModel.OrderHeader.Id, includeProperties: "Product");
+
+            //stripe settings
+            var domain = "https://localhost:7101/";
+            var options = new SessionCreateOptions
+            {
+                PaymentMethodTypes = new List<string>()
+                {
+                    "card",
+                },
+                LineItems = new List<SessionLineItemOptions>(),
+                Mode = "payment",
+                SuccessUrl = domain + $"admin/order/PaymentConfirmation?orderHeaderid={OrderViewModel.OrderHeader.Id}",
+                CancelUrl = domain + $"admin/order/details?orderId={OrderViewModel.OrderHeader.Id}",
+            };
+
+            foreach (var item in OrderViewModel.OrderDetail)
+            {
+                var sessionLineItem = new SessionLineItemOptions
+
+                {
+                    PriceData = new SessionLineItemPriceDataOptions
+                    {
+                        UnitAmount = (long)(item.Price * 100), // 20.00 we need to convert to 2000, its double so we have to convert to long
+                        Currency = "pln",
+                        ProductData = new SessionLineItemPriceDataProductDataOptions
+                        {
+                            Name = item.Product.Title
+                        },
+
+                    },
+                    Quantity = item.Count,
+                };
+                options.LineItems.Add(sessionLineItem);
+
+            }
+
+            var service = new SessionService();
+            Session session = service.Create(options); //here session is created
+            _unitOW.OrderHeader.UpdateStripePayId(OrderViewModel.OrderHeader.Id, session.Id, session.PaymentIntentId);
+            _unitOW.Save();
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303); 
+             
+        }
+
+        public IActionResult PaymentConfirmation(int orderHeaderid)
+        {
+            OrderHeader orderHeader = _unitOW.OrderHeader.GetFirstOrDefault(u => u.Id == orderHeaderid);
+            if (orderHeader.PaymentStatus == SD.PaymentStatusDelayedPaymenet)
+            {
+                //then we want to create a new session service
+                var service = new SessionService();
+                Session session = service.Get(orderHeader.SessionId); //here session is created 
+                //Upper, we retrieve OH from DB based on the id, in order to check stripe status
+                //now we want to check status to make sure if the payment is actually done
+                //only then we will aprove the status
+                if (session.PaymentStatus.ToLower() == "paid")
+                {
+
+                    _unitOW.OrderHeader.UpdateStatus(orderHeaderid, orderHeader.OrderStatus, SD.PaymentStatusAccepted);
+                    _unitOW.Save();
+                }
+            }
+
+            return View(orderHeaderid);
         }
         [HttpPost]
         [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
